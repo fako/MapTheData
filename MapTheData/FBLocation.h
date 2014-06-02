d7edcc8a252ad4a9561f704ee9359f5d293f2b1c
fako <email@fakoberkers.nl>
2014-06-02 22:06:56 +0200

    Make simple call to fetch station data.

---
+++ MapTheData/FBLocation.h
@@ -0,0 +1,13 @@
+//
+//  FBLocation.h
+//  MapTheData
+//
+//  Created by Fako Berkers on 6/2/14.
+//  Copyright (c) 2014 Fako Berkers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FBLocation : NSObject
+
+@end
---
+++ MapTheData/FBLocation.m
@@ -0,0 +1,13 @@
+//
+//  FBLocation.m
+//  MapTheData
+//
+//  Created by Fako Berkers on 6/2/14.
+//  Copyright (c) 2014 Fako Berkers. All rights reserved.
+//
+
+#import "FBLocation.h"
+
+@implementation FBLocation
+
+@end
